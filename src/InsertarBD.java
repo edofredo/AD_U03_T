f966743e77e0042f3de368d645fb1d75dca1be40
Cristian <Cristian@desktop-kofosec>
2021-11-02 12:19:21 +0100

    Initial commit

---
+++ src/InsertarBD.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Cristian
+ */
+public class InsertarBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
